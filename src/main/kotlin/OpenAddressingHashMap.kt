885576ea4f1877e300e9c9b9b0b4fa1a8b7a4c3f
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2025-04-02 14:56:27 +0400

    Initial commit

---
+++ src/main/kotlin/OpenAddressingHashMap.kt
@@ -0,0 +1,29 @@
+class OpenAddressingHashMap<K, V> : Map<K, V> {
+    override val size: Int
+        get() = TODO("Not yet implemented")
+
+    override val keys: Set<K>
+        get() = TODO("Not yet implemented")
+
+    override val values: Collection<V>
+        get() = TODO("Not yet implemented")
+
+    override val entries: Set<Map.Entry<K, V>>
+        get() = TODO("Not yet implemented")
+
+    override fun isEmpty(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun containsKey(key: K): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun containsValue(value: V): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun get(key: K): V? {
+        TODO("Not yet implemented")
+    }
+}
