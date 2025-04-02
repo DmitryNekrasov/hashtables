a65307a3e6f34c72154ce93ed858fa228a30db22
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2025-04-02 14:58:15 +0400

    Make OpenAddressingHashMap mutable

--- src/main/kotlin/OpenAddressingHashMap.kt
+++ src/main/kotlin/OpenAddressingHashMap.kt
@@ -1,14 +1,31 @@
-class OpenAddressingHashMap<K, V> : Map<K, V> {
+class OpenAddressingHashMap<K, V> : MutableMap<K, V> {
+
+    override val keys: MutableSet<K>
+        get() = TODO("Not yet implemented")
+
+    override val values: MutableCollection<V>
+        get() = TODO("Not yet implemented")
+
+    override val entries: MutableSet<MutableMap.MutableEntry<K, V>>
+        get() = TODO("Not yet implemented")
+
+    override fun put(key: K, value: V): V? {
+        TODO("Not yet implemented")
+    }
+
+    override fun remove(key: K): V? {
+        TODO("Not yet implemented")
+    }
+
+    override fun putAll(from: Map<out K, V>) {
+        TODO("Not yet implemented")
+    }
+
+    override fun clear() {
+        TODO("Not yet implemented")
+    }
+
     override val size: Int
-        get() = TODO("Not yet implemented")
-
-    override val keys: Set<K>
-        get() = TODO("Not yet implemented")
-
-    override val values: Collection<V>
-        get() = TODO("Not yet implemented")
-
-    override val entries: Set<Map.Entry<K, V>>
         get() = TODO("Not yet implemented")
 
     override fun isEmpty(): Boolean {
